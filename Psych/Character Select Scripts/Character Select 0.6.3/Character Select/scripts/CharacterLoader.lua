9a520157f202dc658ec0cd128bea4f6514d6985d
xarlosyosoy-cpu <xarlosyosoy@gmail.com>
2026-09-10 15:57:09 -0400

    Mejoras al CharacterLoader.lua
    Mejoras realizadas:
    
    - val ahora es local (evita variables globales)
    - dadName verificado con or '' (evita errores si es nil)
    - checkFileExists antes de loadSong (evita crasheos)
    - debugPrint si la canción no se encuentra
    - elseif en lugar de else if (sintaxis correcta)
    - Verificación de dadName en gfCheck()
    
    Estos cambios hacen el script más robusto y evitan errores en tiempo de ejecución.

--- Psych/Character Select Scripts/Character Select 0.6.3/Character Select/scripts/CharacterLoader.lua
+++ Psych/Character Select Scripts/Character Select 0.6.3/Character Select/scripts/CharacterLoader.lua
@@ -1,4 +1,4 @@
--- V2.5.5
+-- V2.5.6 (mejorado por Xarlosgammer0FNF)
 -- For Version 0.6.3
 -- script made by camlikeskirby (Username on a lot of sites)
 -- Loads in the characters chosen in AutoCharSelect.lua while doing some other stuff
@@ -13,40 +13,57 @@ local removeChangeCharacterEventGF = true
 local savebf = ''
 local savegf = ''
 local ce = false
-local pixelStage = false 
+local pixelStage = false
 
 function onCreatePost()
-initSaveData('autoCharacterSelect')
-
-savebf = getDataFromSave('autoCharacterSelect', 'bf', 'bf') 
-savegf = getDataFromSave('autoCharacterSelect', 'gf', 'gf')
-pixelStage = getPropertyFromClass('PlayState', 'isPixelStage')
-
-if not pixelStage then
-if bfChange then triggerEvent("Change Character", "0", savebf) end
-if gfChange then gfCheck() end
-end
-ce = true
+    initSaveData('autoCharacterSelect')
+    
+    savebf = getDataFromSave('autoCharacterSelect', 'bf', 'bf')
+    savegf = getDataFromSave('autoCharacterSelect', 'gf', 'gf')
+    pixelStage = getPropertyFromClass('PlayState', 'isPixelStage')
+    
+    if not pixelStage then
+        if bfChange then triggerEvent("Change Character", "0", savebf) end
+        if gfChange then gfCheck() end
+    end
+    ce = true
 end
 
 function onEvent(event, value1, value2, strumTime)
     if event == 'Change Character' and ce and not pixelStage then
-    ce = false
-    val = string.lower(value1)
-    if (val == '1' or val == 'dad' or val == 'opponent') and (stringStartsWith(dadName, 'gf-') or dadName == 'gf') and gfChange and removeChangeCharacterEventGF then triggerEvent("Change Character", "1", savegf) 
-    else if (val == '2' or val == 'gf' or val == 'girlfriend') and gfChange and removeChangeCharacterEventGF then triggerEvent("Change Character", "2", savegf) 
-    else if bfChange and removeChangeCharacterEvent then triggerEvent("Change Character", "0", savebf) end
+        ce = false
+        local val = string.lower(value1)
+        local currentDadName = dadName or ''
+        
+        if (val == '1' or val == 'dad' or val == 'opponent') and 
+           (stringStartsWith(currentDadName, 'gf-') or currentDadName == 'gf') and 
+           gfChange and removeChangeCharacterEventGF then
+            triggerEvent("Change Character", "1", savegf)
+        elseif (val == '2' or val == 'gf' or val == 'girlfriend') and 
+               gfChange and removeChangeCharacterEventGF then
+            triggerEvent("Change Character", "2", savegf)
+        elseif bfChange and removeChangeCharacterEvent then
+            triggerEvent("Change Character", "0", savebf)
+        end
+        ce = true
     end
-    end
-    ce = true
-   end
 end
 
-function onUpdate()
-if keyboardJustPressed('SHIFT') and keyboardJustPressed('ONE') then loadSong('Character Select', 1) end
+function onUpdate(elapsed)
+    if keyboardJustPressed('SHIFT') and keyboardJustPressed('ONE') then
+        if checkFileExists('data/Character Select/Character Select.json') then
+            loadSong('Character Select', 1)
+        else
+            debugPrint('[CharacterSelect] Canción no encontrada')
+        end
+    end
 end
 
-function gfCheck() 
-if stringStartsWith(dadName, 'gf-') or dadName == 'gf' then triggerEvent("Change Character", "1", savegf) 
-else triggerEvent("Change Character", "2", savegf)
-end end
+function gfCheck()
+    local currentDadName = dadName or ''
+    if stringStartsWith(currentDadName, 'gf-') or currentDadName == 'gf' then
+        triggerEvent("Change Character", "1", savegf)
+    else
+        triggerEvent("Change Character", "2", savegf)
+    end
+end
